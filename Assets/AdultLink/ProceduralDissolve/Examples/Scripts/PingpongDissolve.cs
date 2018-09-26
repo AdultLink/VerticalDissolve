046ab899f18e7faceb2448dbb79ee58e08d4bed7
Willy Angel <gguillermix@gmail.com>
2018-09-26 03:44:53 +0200

    Monkeys examples

---
+++ Assets/AdultLink/ProceduralDissolve/Examples/Scripts/PingpongDissolve.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingpongDissolve : MonoBehaviour {
+
+	// Use this for initialization
+	public float minValue;
+	public float maxValue;
+	public float freq;
+	public Material mat;
+	private float fill = 0f;
+	private float range;
+	void Start () {
+		range = maxValue - minValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fill = (range / 2f) * (Mathf.Sin(freq * Time.time) + 1f ) + minValue;
+		mat.SetFloat("_Fillpercentage", fill);
+	}
+}
